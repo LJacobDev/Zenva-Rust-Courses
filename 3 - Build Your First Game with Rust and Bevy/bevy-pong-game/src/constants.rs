caee3a4dd471b983dc6f9e5fae5c82877616e40f
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-12-26 18:46:55 -0500

    define game constants

---
+++ 3 - Build Your First Game with Rust and Bevy/bevy-pong-game/src/constants.rs
@@ -0,0 +1,7 @@
+pub const BALL_SIZE: f32 = 5.;
+pub const PADDLE_WIDTH: f32 = 10.;
+pub const PADDLE_HEIGHT: f32 = 50.;
+
+pub const BALL_SPEED: f32 = 5.;
+pub const PADDLE_SPEED: f32 = 5.;
+pub const BOUNDARY_HEIGHT: f32 = 20.;
--- 3 - Build Your First Game with Rust and Bevy/bevy-pong-game/src/main.rs
+++ 3 - Build Your First Game with Rust and Bevy/bevy-pong-game/src/main.rs
@@ -1,4 +1,5 @@
 mod components;
+mod constants;
 fn main() {
 
 
