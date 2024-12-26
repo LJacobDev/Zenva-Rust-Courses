89d59002d8ac4c2eaf1c596f6b9783f690ef9ca4
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-12-26 16:39:23 -0500

    add notes about options and results

---
+++ 2 - Data Structures with Rust/option-and-result-for-data-safety/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+
+
+    /*
+    
+        Option enum:
+
+            a way to handle potentially null or missing values with something that is able to represent it while not actually being null, avoiding crashes
+
+        Result enum:
+
+            a way to handle things that might potentially succeed or fail, where they can be detected and handled without crashing the program
+
+
+
+
+        the code was to make a 'divide_by' function that gives none if you use 0 as the divisor
+
+        and a read_file that returns Ok(filecontentsstring) if valid filename and Err(string) if not valid file name, the return type of that function was Result<String, String>
+
+        they were very self explanatory after having seen about Option and Result enums elsewhere a few times
+
+
+     */
+
+
+}
