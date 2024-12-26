ae0cefb1e5e5d817c72c94aa5192759a4994ed04
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-12-26 17:26:22 -0500

    Initial program structure with empty files as scaffolding

--- 3 - Build Your First Game with Rust and Bevy/bevy-pong-game/src/main.rs
+++ 3 - Build Your First Game with Rust and Bevy/bevy-pong-game/src/main.rs
@@ -1,4 +1,48 @@
 fn main() {
-    println!("Hello, world!");
-    println!("Bevy has been installed!");
+
+
+    /*
+        This pong game is going to have a program structure with the intention to modularize it so that it has improved organization, maintainability, and scalability
+
+        it will be following the ECS Entity-component-system paradigm
+
+        so first a components.rs file is made that defines ECS components in the game, like player, ball, and paddle.
+
+        These components will be centralized there, making a single place to manage all the data types attached to entities, making it easier to track and modify
+
+
+        Next, a systems directory is made
+
+        This contains all the individual modules for different systems in the game
+
+        each module handles a specific aspect of the game logic, ensuring separation of concerns
+
+        mod.rs file is created in it as it will export the modules for main.rs to use, just by referencing the system module
+
+
+        Next we create ball.rs which contains systems related to the ball's behaviour, like spawning, movement, and scoring detection
+
+        And paddle.rs contains systems for input from player one and player two
+
+        scoreboard.rs handles the systems related to the scoreboard, such as updating and displaying scores
+
+        collision.rs contains systems that detect collisions and interaction between the ball and paddles
+
+        Next, in the src directory, a bundles.rs is made.  This defines the entity bundles, which are components that are frequently used together.  For example, a ball bundle might include ball, position
+
+        using these bundles helps streamline entity creation, ensuring that all the required components are included
+
+        lastly, there is constants.rs which contains the constant values used throughout the game, like speed, dimension, other config settings we might need.  Having them centralized here helps to change the values and making sure it propagates throughout the code base from one place.
+
+
+        This program structure allows us to break down the modules according to their functionality
+
+        By separating components, system and bundles, it means that each file has a clear responsibility and it helps manage complexity, and reduces likelihood of errors
+
+        This will be really useful as the game grows in complexity,
+
+        and new features and systems can be added without disrupting the existing code that much
+
+     */
+
 }
