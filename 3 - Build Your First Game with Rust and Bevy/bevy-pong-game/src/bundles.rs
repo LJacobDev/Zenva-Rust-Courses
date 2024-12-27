e720be47151b5ff61af138f0af8014aa16da85df
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-12-26 19:14:12 -0500

    define bundles

---
+++ 3 - Build Your First Game with Rust and Bevy/bevy-pong-game/src/bundles.rs
@@ -0,0 +1,63 @@
+use crate::components::*;
+use crate::constants::*;
+use bevy::math::Vec2;
+use bevy::prelude::*;
+
+/*
+    Bundles in Bevy are collections of components that are often used together
+*/
+
+#[derive(Bundle)]
+pub struct BallBundle {
+    pub ball: Ball,
+    pub shape: Shape,
+    pub velocity: Velocity,
+    pub position: Position,
+}
+
+impl BallBundle {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self {
+            ball: Ball,
+            shape: Shape(Vec2::new(BALL_SIZE, BALL_SIZE)),
+            velocity: Velocity(Vec2::new(x, y)),
+            position: Position(Vec2::new(0., 0.)),
+        }
+    }
+}
+
+#[derive(Bundle)]
+pub struct PaddleBundle {
+    paddle: Paddle,
+    shape: Shape,
+    velocity: Velocity,
+    position: Position,
+}
+
+impl PaddleBundle {
+    pub fn new(s: f32, y: f32) -> Self {
+        Self {
+            paddle: Paddle,
+            shape: Shape(Vec2::new(PADDLE_HEIGHT, PADDLE_WIDTH)),
+            position: Position(Vec2::new(x, y)),
+            velocity: Velocity(Vec2::new(0., 0.)),
+        }
+    }
+}
+
+#[derive(Bundle)]
+pub struct BoundaryBundle {
+    pub boundary: Boundary,
+    pub shape: Shape,
+    pub position: Position,
+}
+
+impl BoundaryBundle {
+    pub fn new(x: f32, y: f32, width: f32) -> Self {
+        Self {
+            boundary: Boundary,
+            shape: Shape(Vec2::new(width, BOUNDARY_HEIGHT)),
+            position: Position(Vec2::new(x, y)),
+        }
+    }
+}
