0d720dc1b45357315ef5d3907b96a7aca2725b1a
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2025-01-10 22:54:16 -0500

    Add Actor and message Handler implementations for WsSessionManager

--- 7 - WebSocket Actrix server Collaborative Document Editing System/rust-web-app-project-collaborative-document-editing/src/session_manager/mod.rs
+++ 7 - WebSocket Actrix server Collaborative Document Editing System/rust-web-app-project-collaborative-document-editing/src/session_manager/mod.rs
@@ -1,7 +1,8 @@
-use std::collections::HashSet;
+use crate::ws_actor::{BroadcastMessage, Connect, Disconnect, WebSocket};
 use actix::{Actor, Addr, Context, Handler};
+use std::{collections::HashSet, hash::Hash};
 
-pub struct WsSessionManager{
+pub struct WsSessionManager {
     /// A set to track the addresses of connected WebSocket actors
     sessions: HashSet<Addr<WebSocket>>,
 
@@ -11,5 +12,34 @@ pub struct WsSessionManager{
 }
 
 impl WsSessionManager {
+    pub(crate) fn new() -> Self {
+        let default_text = "{\"ops\":[{\"insert\":\"Write \"},{\"attributes\":{\"underline\":true}, \"insert\":\"here\"},{\"insert\":\" some \"},{\"attributes\":{\"bold\":true},\"insert\":\"text\"},{\"insert\":\"!\"}]}";
 
+        
+
+        Self {
+            sessions: HashSet::new(),
+            last_text: default_text.to_string(),
+        }
+    }
+}
+
+
+impl Actor for WsSessionManager {
+    type Context = Context<Self>;   //standard context type for Actrix actors
+}
+
+// handles the connect message that signals a new WS client has connected
+impl Handler<Connect> for WsSessionManager {
+    type Result = (); // no specific result is returned after handling the message
+
+    fn handle(&mut self, msg: Connext, _: &mut Context<Self>){
+
+        println!("New Client Connected");
+
+        let addr = msg.addr;
+
+        //insert the address of this new client into the sessions hash set so the Ws Session Manager can track them all
+        self.sessions.insert(addr.clone());
+    }
 }
